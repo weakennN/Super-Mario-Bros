de886762b96fb351b60a8ae5c27824cb1a74d11c
weakennN <lubomedi03@gmail.com>
2021-09-06 20:58:26 +0300

    fixed sound bug

--- src/Game/SoundEffects/SoundManager.java
+++ src/Game/SoundEffects/SoundManager.java
@@ -14,34 +14,23 @@ public class SoundManager {
     private static AudioClip audioClip;
 
     public static void playMarioBackgroundTheme() {
-
-      /*  File file = new File(Sounds.superMarioBackgroundMusic);
+        File file = new File(Sounds.superMarioBackgroundMusic);
         media = new Media(Paths.get(file.getAbsolutePath()).toUri().toString());
         mediaPlayer = new MediaPlayer(media);
         mediaPlayer.setVolume(0.04);
         mediaPlayer.setAutoPlay(true);
         mediaPlayer.play();
-
-       */
     }
 
     public static void playSound(String sound) {
-
-        /*File file = new File(sound);
-
+        File file = new File(sound);
         audioClip = new AudioClip(file.toURI().toString());
         audioClip.setVolume(0.05);
         audioClip.play();
-
-         */
     }
 
     public static void stop() {
-
-        /*mediaPlayer.stop();
+        mediaPlayer.stop();
         audioClip.stop();
-
-         */
     }
-
 }
